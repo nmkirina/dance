2d90fe4e0b97a73ca3f35d080cb89b1a4ca564c7
Irina <sokolovaie@altarix.ru>
2017-06-29 19:04:48 +0300

    Post request, dance list

--- app/dances/dances.component.ts
+++ app/dances/dances.component.ts
@@ -1,21 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
     selector: 'dances',
-    templateUrl: "app/dances/dances.component.html",
+    templateUrl: "app/html-template/item-with-picture.component.html",
     styles: [`#dances>img{
                     width: 300px;
                     height: 200px;    
                 }
                 #dances{
                     position: relative;
-                }`]
+                }`],
+    providers: [HttpService]
 })
 export class DancesComponent implements OnInit {
-    constructor(){}
+    
+    items: any;
+    
+    constructor(private httpService: HttpService){}
     ngOnInit(){
+        this.httpService.getDances().subscribe(error => {console.log(error)});
         
-        console.log('dances');
+        this.httpService.getDances().subscribe((data) => {this.items = data;});
     }
+    
 }
 
--- app/http.service.ts
+++ app/http.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
-import {Response} from '@angular/http';
+import {Response, Headers} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
@@ -15,5 +15,16 @@ export class HttpService{
        return this.http.get('http://localhost/server/getFactorial.php?number=' + num)
                        .map((resp:Response)=>resp.json())
                        .catch((error:any) =>{return Observable.throw(error);});
+       
+   }
+   
+   getDances(){
+      
+      let headers =  new Headers({ 'Content-Type': 'application/json;charset=utf-8' });
+      let result: Observable<Response> = this.http.post('http://serv.dance.local/api/dances', null, {headers: headers})
+                                                .map((resp:Response)=>resp.json())
+                                                .catch((error:any) =>{return Observable.throw(error);});
+                       
+      return result;
    }
 }
