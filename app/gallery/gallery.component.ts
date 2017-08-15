742f139f07723dbd4abaf63136d2b12dc44bcc8a
Irina <sokolovaie@altarix.ru>
2017-08-15 18:34:46 +0300

    Gallery component corrected for image-gallery

--- app/gallery/gallery.component.ts
+++ app/gallery/gallery.component.ts
@@ -1,24 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpService } from '../http.service';
-
+import { Component } from '@angular/core';
 @Component({
     selector: 'gallery1',
     template: `<div id="gallery1">
-                  <gallery [metadataUri]="'./app/gallery/data.json'"></gallery>  
+                  <gallery [metadataUri]="'./uploads/data.json'"></gallery>
                 </div>
-               `,
-    providers: [ HttpService ]
+               `
 })
-export class GalleryComponent implements OnInit {
-    items: any;
-    images: any;
-    
-    constructor(private httpService: HttpService){}
-    ngOnInit(){
-        
-//        this.httpService.getGallery().subscribe((data) => 
-//        {
-//            this.items = data.result;
-//        });
-    }
+export class GalleryComponent{
+    constructor(){}
 }
