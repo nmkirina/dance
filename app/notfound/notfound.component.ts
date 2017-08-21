96fad9da6d4d0a7ae5ca814bda91b428beed5ddf
Irina <sokolovaie@altarix.ru>
2017-08-21 11:56:15 +0300

    404 page

--- app/notfound/notfound.component.ts
+++ app/notfound/notfound.component.ts
@@ -2,8 +2,20 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'notfound',
-    template: '<div id="notfound">Not found</div>',
-    styles: [`#staff{color: red;}`]
+    template: `<div id="oops">
+                    <h1>Страница не найдена</h1>
+                    <img src="img/oops.jpg">
+                    
+                </div>`,
+    styles: [`#oops > h1 {
+                text-align: center;
+              }  
+              #oops > img{
+                    position: relative;
+                    left: 25%;
+                    top: 5%;
+              }
+                `]
 })
 export class NotfoundComponent implements OnInit {
     constructor(){}
